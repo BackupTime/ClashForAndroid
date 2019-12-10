e97a2d58c88ad7e6a1949168c879b5ad603477e7
Kr328 <39107975+Kr328@users.noreply.github.com>
2019-12-10 13:10:56 +0800

    add crashlytics

--- app/src/main/java/com/github/kr328/clash/MainApplication.kt
+++ app/src/main/java/com/github/kr328/clash/MainApplication.kt
@@ -2,6 +2,7 @@ package com.github.kr328.clash
 
 import android.app.Application
 import android.content.Context
+import com.github.kr328.clash.core.Constants
 import com.github.kr328.clash.core.utils.Log
 import com.google.firebase.FirebaseApp
 
@@ -28,6 +29,29 @@ class MainApplication : Application() {
             Log.i("Registered")
         } catch (e: IllegalStateException) {
             Log.i("Already registered")
+        }
+
+        Log.handler = object: Log.LogHandler {
+            override fun info(message: String, throwable: Throwable?) {
+                android.util.Log.i(Constants.TAG, message, throwable)
+            }
+
+            override fun warn(message: String, throwable: Throwable?) {
+
+            }
+
+            override fun error(message: String, throwable: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun wtf(message: String, throwable: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun debug(message: String, throwable: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
         }
     }
 }
