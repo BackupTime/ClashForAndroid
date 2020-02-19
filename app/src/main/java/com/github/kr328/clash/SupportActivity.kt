17fd5954a0537ebfd7c0131ad71a51a77e322e0d
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-02-19 22:57:10 +0800

    add support click action

--- app/src/main/java/com/github/kr328/clash/SupportActivity.kt
+++ app/src/main/java/com/github/kr328/clash/SupportActivity.kt
@@ -1,6 +1,12 @@
 package com.github.kr328.clash
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.core.content.getSystemService
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_support.*
 
 class SupportActivity: BaseActivity() {
@@ -19,13 +25,19 @@ class SupportActivity: BaseActivity() {
             option(
                 title = getString(R.string.clash),
                 summary = getString(R.string.clash_url)) {
-
+                onClick {
+                    startActivity(Intent(Intent.ACTION_VIEW)
+                        .setData(Uri.parse(getString(R.string.clash_url))))
+                }
             }
             option(
                 title = getString(R.string.clash_for_android),
                 summary = getString(R.string.clash_for_android_url)
             ) {
-
+                onClick {
+                    startActivity(Intent(Intent.ACTION_VIEW)
+                        .setData(Uri.parse(getString(R.string.clash_for_android_url))))
+                }
             }
 
             category(text = getString(R.string.contacts))
@@ -34,13 +46,22 @@ class SupportActivity: BaseActivity() {
                 title = getString(R.string.email),
                 summary = getString(R.string.email_url)
             ) {
+                onClick {
+                    val data =
+                        ClipData.newPlainText("email", getText(R.string.email_url))
+                    getSystemService<ClipboardManager>()?.setPrimaryClip(data)
 
+                    Snackbar.make(rootView, getText(R.string.copied), Snackbar.LENGTH_SHORT).show()
+                }
             }
             option(
                 title = getString(R.string.github_issues),
                 summary = getString(R.string.github_issues_url)
             ) {
-
+                onClick {
+                    startActivity(Intent(Intent.ACTION_VIEW)
+                        .setData(Uri.parse(getString(R.string.github_issues_url))))
+                }
             }
 
             if ( resources.configuration.locales.get(0)
@@ -49,7 +70,10 @@ class SupportActivity: BaseActivity() {
                     title = getString(R.string.telegram_channel),
                     summary = getString(R.string.telegram_channel_url)
                 ) {
-
+                    onClick {
+                        startActivity(Intent(Intent.ACTION_VIEW)
+                            .setData(Uri.parse(getString(R.string.telegram_channel_url))))
+                    }
                 }
             }
         }
