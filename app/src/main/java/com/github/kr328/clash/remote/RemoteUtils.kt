f7dddb41fa5c641683b71fda5e7e01762093c7cf
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-02-25 20:53:26 +0800

    add crash reporter

--- app/src/main/java/com/github/kr328/clash/remote/RemoteUtils.kt
+++ app/src/main/java/com/github/kr328/clash/remote/RemoteUtils.kt
@@ -1,40 +1,57 @@
 package com.github.kr328.clash.remote
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import com.github.kr328.clash.ApkBrokenActivity
 import com.github.kr328.clash.service.Constants
 import com.github.kr328.clash.service.ServiceStatusProvider
+import com.github.kr328.clash.service.util.intent
 
 object RemoteUtils {
     fun detectClashRunning(context: Context): Boolean {
-        val authority = Uri.Builder()
-            .scheme("content")
-            .authority("${context.packageName}${Constants.STATUS_PROVIDER_SUFFIX}")
-            .build()
+        try {
+            val authority = Uri.Builder()
+                .scheme("content")
+                .authority("${context.packageName}${Constants.STATUS_PROVIDER_SUFFIX}")
+                .build()
 
-        val pong = context.contentResolver.call(
-            authority,
-            ServiceStatusProvider.METHOD_PING_CLASH_SERVICE,
-            null,
-            null
-        )
+            val pong = context.contentResolver.call(
+                authority,
+                ServiceStatusProvider.METHOD_PING_CLASH_SERVICE,
+                null,
+                null
+            )
 
-        return pong != null
+            return pong != null
+        }
+        catch (e: IllegalArgumentException) {
+            context.startActivity(ApkBrokenActivity::class.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+
+            return false
+        }
     }
 
     fun getCurrentClashProfileName(context: Context): String? {
-        val authority = Uri.Builder()
-            .scheme("content")
-            .authority("${context.packageName}${Constants.STATUS_PROVIDER_SUFFIX}")
-            .build()
+        try {
+            val authority = Uri.Builder()
+                .scheme("content")
+                .authority("${context.packageName}${Constants.STATUS_PROVIDER_SUFFIX}")
+                .build()
 
-        val pong = context.contentResolver.call(
-            authority,
-            ServiceStatusProvider.METHOD_PING_CLASH_SERVICE,
-            null,
-            null
-        )
+            val pong = context.contentResolver.call(
+                authority,
+                ServiceStatusProvider.METHOD_PING_CLASH_SERVICE,
+                null,
+                null
+            )
 
-        return pong?.getString("name")
+            return pong?.getString("name")
+        }
+        catch (e: java.lang.IllegalArgumentException) {
+            context.startActivity(ApkBrokenActivity::class.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+
+            return null
+        }
     }
 }
