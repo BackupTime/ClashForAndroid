04eed1a7685b72509b8b0a1d75732c20b809dbd1
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-04-13 12:00:48 +0800

    broadcast with permission

--- service/src/main/java/com/github/kr328/clash/service/util/BroadcastUtils.kt
+++ service/src/main/java/com/github/kr328/clash/service/util/BroadcastUtils.kt
@@ -2,10 +2,12 @@ package com.github.kr328.clash.service.util
 
 import android.content.Context
 import android.content.Intent
+import com.github.kr328.clash.common.Permissions
 import com.github.kr328.clash.common.ids.Intents
 
 fun Context.sendBroadcastSelf(intent: Intent) {
-    this.sendBroadcast(intent.setPackage(this.packageName))
+    this.sendBroadcast(intent.setPackage(this.packageName),
+        Permissions.PERMISSION_ACCESS_CLASH)
 }
 
 fun Context.broadcastProfileChanged() {
