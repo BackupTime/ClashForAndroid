861d5fa871e6e8c983415d4f307a3e19d3ff0d33
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-04-15 11:07:02 +0800

    Update tun2socket

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
         this["gMinSdkVersion"] = 24
         this["gTargetSdkVersion"] = 29
 
-        this["gVersionCode"] = 10202
-        this["gVersionName"] = "1.2.2"
+        this["gVersionCode"] = 10203
+        this["gVersionName"] = "1.2.3"
 
         this["gKotlinVersion"] = kotlinVersion
         this["gKotlinCoroutineVersion"] = "1.3.5"
--- core/src/main/golang/go.mod
+++ core/src/main/golang/go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/Dreamacro/clash v0.0.0 // local
-	github.com/kr328/tun2socket v0.0.0-20200414165956-abd6279de46b
+	github.com/kr328/tun2socket v0.0.0-20200415021819-256b721ac9a4
 	github.com/miekg/dns v1.1.29
 )
 
