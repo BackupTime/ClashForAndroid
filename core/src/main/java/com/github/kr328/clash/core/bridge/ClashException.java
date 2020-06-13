160b96d829c007d0837dfc124079e101fb386119
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-06-13 19:03:42 +0800

    fix crash on find method ClashException

--- core/src/main/java/com/github/kr328/clash/core/bridge/ClashException.java
+++ core/src/main/java/com/github/kr328/clash/core/bridge/ClashException.java
@@ -1,6 +1,9 @@
 package com.github.kr328.clash.core.bridge;
 
+import androidx.annotation.Keep;
+
 public class ClashException extends Exception {
+    @Keep
     public ClashException(String msg) {
         super(msg);
     }
